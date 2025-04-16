8045ad9eb516a0d839a68c7a3aacbf03ad39bd93
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-04-16 16:56:48 +0200

    feat: add bonus 3

--- src/pages/Lista-post.jsx
+++ src/pages/Lista-post.jsx
@@ -1,5 +1,31 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
 export default function ListaPost() {
+
+    const endpoint = ' https://jsonplaceholder.typicode.com/posts'
+
+    const [postList, setPostList] = useState([]);
+
+    useEffect(() => {
+        axios.get(endpoint)
+            .then(res => setPostList(res.data))
+            .catch(err => {
+                alert('Indirizzo errato')
+            })
+    }, [])
+
     return (
-        <h2>Lista dei post</h2>
+        <section>
+            <h2>Lista dei post</h2>
+            <ul>
+                {postList.map(post =>
+                    <li key={post.id}>
+                        <h4>{post.title}</h4>
+                        <p>{post.body}</p>
+                    </li>
+                )}
+            </ul>
+        </section>
     )
 }
