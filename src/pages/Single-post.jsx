90d45aa75e0eebcc4579008a57c47a163e648e27
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-04-17 19:24:14 +0200

    feat: add bonus

--- src/pages/Single-post.jsx
+++ src/pages/Single-post.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import { useParams, useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom"
 import axios from "axios"
 
 const endpoint = 'https://jsonplaceholder.typicode.com/posts'
@@ -29,6 +30,11 @@ export default function SinglePost() {
             })
     }, [id])
 
+    const parseId = parseInt(id);
+    const prevPost = parseId - 1 >= 1 ? parseId - 1 : id;
+    const nextPost = parseId + 1 <= 100 ? parseId + 1 : id;
+
+
     if (error) {
         return <div>Error...</div>
     }
@@ -42,6 +48,8 @@ export default function SinglePost() {
             <h2>{singlePost.title}</h2>
             <p>{singlePost.body}</p>
             <button onClick={() => navigate(-1)}>Torna alla pagina precedente</button>
+            <button disabled={id <= 1}> <Link to={`/lista-post/${prevPost}`} >Post precedente</Link></button>
+            <button disabled={id >= 100}> <Link to={`/lista-post/${nextPost}`} >Post successivo</Link></button>
         </section>
     )
 }
